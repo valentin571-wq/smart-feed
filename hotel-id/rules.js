cc0eba18fa2c667a5819863600fe08cfa4d60478
valentin571-wq <valentin571@gmail.com>
2026-03-15 23:00:50 +0200

    Create rules.js

---
+++ hotel-id/rules.js
@@ -0,0 +1,29 @@
+const hotelId = document.body.dataset.hotel;
+
+const HOTEL_CONFIG = {
+  monopoly: {
+    prices: { 1:220, 2:250, 3:250, 4:300 },
+    micDejunPretAdult: 50,
+    micDejunPretCopil: 5,
+    hasBreakfast: true,
+    hasOnlinePayment: true,
+    hasCashPayment: true,
+    blockedRanges: [
+      { from: "2026-03-10", to: "2026-03-16" }
+    ]
+  },
+
+  brasov: {
+    prices: { 1:220, 2:280, 3:280, 4:350 },
+    micDejunPretAdult: 60,
+    micDejunPretCopil: 30,
+    hasBreakfast: false,
+    hasOnlinePayment: false,
+    hasCashPayment: true,
+    blockedRanges: [
+      { from: "2026-02-05", to: "2026-02-12" }
+    ]
+  }
+};
+
+const config = HOTEL_CONFIG[hotelId];
