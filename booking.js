d3a83b28ab3e26468dabc780f63becfbfd6286c8
valentin571-wq <valentin571@gmail.com>
2026-03-15 23:05:01 +0200

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,43 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  /* =====================
-     IDENTITATE HOTEL
-     ===================== */
-  const hotelId = document.body.dataset.hotel;
-
-  /* =====================
-     CONFIG PER HOTEL
-     ===================== */
-  const HOTEL_CONFIG = {
-    monopoly: {
-      prices: { 1:220, 2:250, 3:250, 4:300 },
-      micDejunPretAdult: 50,
-      micDejunPretCopil: 5,
-      hasBreakfast: true,
-      hasOnlinePayment: true,
-      hasCashPayment: true,
-      blockedRanges: [
-        { from: "2026-03-10", to: "2026-03-16" }
-      ]
-    },
-
-    brasov: {
-      prices: { 1:220, 2:280, 3:280, 4:350 },
-      micDejunPretAdult: 60,
-      micDejunPretCopil: 30,
-      hasBreakfast: false,
-       hasOnlinePayment: false,
-       hasCashPayment: true,
-      blockedRanges: [
-        { from: "2026-02-05", to: "2026-02-12" }
-      ]
-    }
-  };
-  
-  
-
-
-  const config = HOTEL_CONFIG[hotelId];
+ 
   if (!config) return;
 
 
